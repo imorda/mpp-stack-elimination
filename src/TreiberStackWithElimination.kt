fcf3f8fef8ec19552b70e286e943cb30fd9dc372
Nikita Koval <ndkoval@ya.ru>
2022-10-05 22:44:56 +0200

    2022 update

---
+++ src/TreiberStackWithElimination.kt
@@ -0,0 +1,29 @@
+package mpp.stackWithElimination
+
+import kotlinx.atomicfu.atomic
+import kotlinx.atomicfu.atomicArrayOfNulls
+
+class TreiberStackWithElimination<E> {
+    private val top = atomic<Node<E>?>(null)
+    private val eliminationArray = atomicArrayOfNulls<Any?>(ELIMINATION_ARRAY_SIZE)
+
+    /**
+     * Adds the specified element [x] to the stack.
+     */
+    fun push(x: E) {
+        TODO("implement me")
+    }
+
+    /**
+     * Retrieves the first element from the stack
+     * and returns it; returns `null` if the stack
+     * is empty.
+     */
+    fun pop(): E? {
+        TODO("implement me")
+    }
+}
+
+private class Node<E>(val x: E, val next: Node<E>?)
+
+private const val ELIMINATION_ARRAY_SIZE = 2 // DO NOT CHANGE IT
